eed953413dee544b2945f64b0436e80ff5b725aa
Jaime Rodriguez <rodriguez.jaime.a@gmail.com>
2019-05-31 22:50:19 -0700

    Updates to first half of HW

--- Main.py
+++ Main.py
@@ -4,35 +4,61 @@ file_name = "03-Python_Homework_PyBank_Resources_budget_data.csv"
 
 with open(file_name, newline='') as csvfile:
     csvreader = csv.reader(csvfile)
-    # row represents each row in the csv file
-    # the type of the row object is a list
+# Initiate the variables for counting the total months, the net PL amount, the previous PL amount
+# the delta from current and previous PL number
     Months = 0
     NetAmt = 0
-    PrevPL = 0
+    PLCurr = 0
+    PLPrev = 0
     Delta = 0
     PLTot = []
+    Count = 0
+    GreatInc = 0
+    GreatIncMonth = ""
+    GreatDec = 0
+    GreatDecMonth = ""
+
     # This will skip the first row of the csv file
     next(csvreader)
     for row in csvreader:
-        if row == 1:
-            PL = int(row[1])
+        if Count == 0:
+            PLCurr = int(row[1])
             Months += 1
-            NetAmt = NetAmt + PL
-            PrevPL = PL
+            NetAmt = NetAmt + PLCurr
+            PLPrev = PLCurr
+            Count = 1
         else:
-            PL = int(row[1])
-            Delta = Delta + (PL - PrevPL)
+            PLCurr = int(row[1])
+            Delta = Delta + (PLCurr - PLPrev)
+            # Compare the delta with what is stored in our greatest increase/decrease variables
+            if (GreatInc < PLCurr - PLPrev):
+                GreatInc = PLCurr - PLPrev
+                GreatIncMonth = row[0]
+            if (GreatDec > PLCurr - PLPrev):
+                GreatDec = PLCurr - PLPrev
+                GreatDecMonth = row[0]
             Months += 1
-            NetAmt = NetAmt + PL
-            PrevPL = PL
-    AvgDelta = Delta / Months
+            NetAmt = NetAmt + PLCurr
+            PLPrev = PLCurr
+            Count += 1
+
+    AvgDelta = Delta / (Count - 1)
+
+
+print("Financial Analysis\n ------------------------")
+print(f"Total Months: {Months}\nTotal: ${NetAmt}\nAverage Change: ${AvgDelta:.2f}\nGreatest Increase"
+      f"in Profits: {GreatIncMonth} (${GreatInc})\nGreatest Decrease in Profits: {GreatDecMonth} (${GreatDec})")
+
+cat > "budget_data.txt"
 
 output_file = "Resources_Budget_output.csv"
 with open(output_file, "w", newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(["Financial Analysis"])
     csvwriter.writerow(["----------------------------"])
-    csvwriter.writerow(["Total Months: ", Months])
-    csvwriter.writerow(["Total: ", NetAmt])
-    csvwriter.writerow(["Average Change: ", AvgDelta])
-    csvwriter.writerow(["Total: ", Delta])
+    csvwriter.writerow(["Total Months:", Months])
+    csvwriter.writerow(["Total:", '${:,.2f}'.format(NetAmt)])
+    csvwriter.writerow(["Average Change:", AvgDelta])
+    csvwriter.writerow(["Greatest Increase in Profits:", GreatIncMonth,GreatInc])
+    csvwriter.writerow(["Greatest Decrease in Profits:", GreatDecMonth,GreatDec])
+    csvwriter.writerow(["Count:", Count])
