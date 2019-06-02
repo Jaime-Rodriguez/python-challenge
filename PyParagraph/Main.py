4717706981ab4d726fb62c045b78e557c4a31432
Jaime Rodriguez <rodriguez.jaime.a@gmail.com>
2019-06-01 23:36:52 -0700

    Starting work on PyParagraph

---
+++ PyParagraph/Main.py
@@ -0,0 +1,10 @@
+text = "Adam Wayne, the conqueror, with his face flung back and his mane like a lion's, stood with his great sword point upwards, the red raiment of his office flapping around him like the red wings of an archangel. And the King saw, he knew not how, something new and overwhelming. The great green trees and the great red robes swung together in the wind. The preposterous masquerade, born of his own mockery, towered over him and embraced the world. This was the normal, this was sanity, this was nature, and he himself, with his rationality, and his detachment and his black frock-coat, he was the exception and the accident a blot of black upon a world of crimson and gold."
+
+word_count = 0
+for i in text:
+    if(i == " "):
+        word_count += 1
+
+    # Increment once more since the last word will not have a space
+word_count += 1
+print(f"Word count: {word_count}")
